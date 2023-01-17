7dae0fd4c2aef2fc9012bfbc84ed26d7f6e407de
Dylan N <120763426+SpotDylan@users.noreply.github.com>
2023-01-17 15:00:20 -0500

    Update scrape.py
    Updated the Web Driver so that it's not using a deprecated instance

--- scrape.py
+++ scrape.py
@@ -11,7 +11,7 @@ class startScrape:
     def scrape(self):
         chrome_options = Options()
         chrome_options.add_experimental_option("detach", True)
-        driver = webdriver.Chrome('drivers/chromedriver.exe', chrome_options=chrome_options)
+        driver = webdriver.Chrome(options = chrome_options) 
         driver.get('https://neuromorpho.org/byspecies.jsp#top')
         humanSection = driver.find_elements(By.XPATH, '/html/body/div[1]/table/tbody/tr[3]/td/table[1]/tbody/tr/td[2]/table/tbody/tr/td/div/form/table/tbody/tr/td[1]/table/tbody/tr[2]/td/div/ul/li[43]')
         #WIP
